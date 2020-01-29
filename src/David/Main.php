83ad600f42865547925c8fe0275db24377ec787b
davidlouis101 <54008552+davidlouis101@users.noreply.github.com>
2020-01-29 14:46:54 +0100

    YT

--- src/David/Main.php
+++ src/David/Main.php
@@ -1,3 +1,37 @@
 <?php
 
-namespace David\Heiraten;
+namespace David\YT;
+ 
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+
+{
+ class Main extends PluginBase implements Listener {
+    public function onEnable() 
+    {
+        $this->getLogger()->info("[YT] Aktiviert.");
+    }
+
+    public $fts;
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool
+    {
+        $fts = "[YT]";
+        $this->fts = $fts;
+        if ($cmd->getName() == "YT") {
+            if ($sender instanceof Player) {
+                if ($sender->hasPermission("yt.cmd")) {
+                    $sender->sendMessage("§4YT§r >> §6Du Brauchst 110 Abos um denn YT Rang Zu Bekommen");
+                    return true;
+                } else {
+                    $sender->sendMessage("Du hast keine Rechte diesen Befehl zu benutzen");
+                    return true;
+                } 
+            }
+            return true;
+         }
+            return true;
+       }
+      }
